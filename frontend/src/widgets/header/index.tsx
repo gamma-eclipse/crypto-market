59b9a0f87fd3e1369e8656a2404b372fa34c10dc
Dispersioned <dispersioned@gmail.com>
2022-08-25 17:30:07 +0300

    feat: create temp nav

--- frontend/src/widgets/header/index.tsx
+++ frontend/src/widgets/header/index.tsx
@@ -1,5 +1,30 @@
+import { styled } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { routes } from 'shared/routes';
+
+const Wrapper = styled('header')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Nav = styled('nav')`
+  display: grid;
+  grid-gap: 15px;
+  grid-auto-flow: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`;
 
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <Wrapper>
+      <Nav>
+        <Link to={routes.home}>Home</Link>
+        <Link to={routes.cart}>Cart</Link>
+      </Nav>
+    </Wrapper>
+  );
 };
