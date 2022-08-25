d2768e27b12ee4829e4ea3878d809b4cde0c6ba8
Dispersioned <dispersioned@gmail.com>
2022-08-25 17:24:40 +0300

    feat: cart page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { Cart } from './cart';
 import { Home } from './home';
 
 export const Routing = () => (
   <Routes>
     <Route index element={<Home />} />
+    <Route path="/cart" element={<Cart />} />
   </Routes>
 );
