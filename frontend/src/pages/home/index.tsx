c9e5ac9f6b654e9c4f781e10eac87dc6e6a40c34
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-08-25 19:25:35 +0300

    Frontend/feature/catalog page (#3)
    * fix: header position
    
    * fix: layout margin
    
    * feat: search bar

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import { IconButton } from '@mui/material';
+import { ReactComponent as SearchIcon } from 'assets/search.svg';
 import { Layout } from 'shared/ui/Layout';
 
+import { ContentLayout, GridArea, Search, Title } from './styles';
+
 export const Home = () => {
-  return <Layout>home page</Layout>;
+  return (
+    <Layout>
+      <ContentLayout>
+        <GridArea name="title">
+          <Title component="h1" variant="h5">
+            Catalog
+          </Title>
+        </GridArea>
+        <GridArea name="search">
+          <Search
+            fullWidth
+            label="Search"
+            InputProps={{
+              endAdornment: (
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </GridArea>
+      </ContentLayout>
+    </Layout>
+  );
 };
---
+++ frontend/src/pages/home/styles.ts
@@ -0,0 +1,35 @@
+import { TextField, Typography, styled } from '@mui/material';
+import { COLORS } from 'shared/colors';
+
+export const ContentLayout = styled('div')`
+  display: grid;
+  grid-gap: 25px;
+  grid-template:
+    'title search' fit-content(100%)
+    'filter content' fit-content(100%)
+    '. content' 1fr / 300px;
+`;
+
+export const GridArea = styled('div', { shouldForwardProp: (propName) => propName !== 'name' })<{ name: string }>`
+  grid-area: ${(props) => props.name};
+  background-color: ${COLORS.bg[100]};
+  border-radius: 7px;
+`;
+
+export const Title = styled(Typography)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+` as typeof Typography;
+
+export const Search = styled(TextField)`
+  .MuiOutlinedInput-notchedOutline {
+    border-width: 3px !important;
+    border-color: ${COLORS[500]} !important;
+  }
+
+  svg {
+    fill: inherit;
+  }
+`;
