54aaa69fa56acac718c80b5ffa994838c48f7e38
Dispersioned <dispersioned@gmail.com>
2022-08-25 19:30:33 +0300

    asdasdasd

--- frontend/src/widgets/footer/index.tsx
+++ frontend/src/widgets/footer/index.tsx
@@ -1,5 +1,19 @@
+import { Typography, styled } from '@mui/material';
 import React from 'react';
 
+const Wrapper = styled('footer')`
+  margin-top: auto;
+  padding: 10px 15px;
+`;
+
+const Copy = styled(Typography)`
+  text-align: center;
+`;
+
 export const Footer = () => {
-  return <footer>Copyright © 2022. All rights reserved</footer>;
+  return (
+    <Wrapper>
+      <Copy>Copyright © 2022. All rights reserved</Copy>
+    </Wrapper>
+  );
 };
