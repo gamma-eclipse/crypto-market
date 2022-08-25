ccad5a14bacb45c46fa4d4acaae75ce142a4f40f
Dispersioned <dispersioned@gmail.com>
2022-08-25 17:26:39 +0300

    feat: create routes config file
    Update routes.ts

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { routes } from 'shared/routes';
 
 import { Cart } from './cart';
 import { Home } from './home';
@@ -7,6 +8,6 @@ import { Home } from './home';
 export const Routing = () => (
   <Routes>
     <Route index element={<Home />} />
-    <Route path="/cart" element={<Cart />} />
+    <Route path={routes.cart} element={<Cart />} />
   </Routes>
 );
