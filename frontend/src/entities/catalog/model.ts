9141f9cd73ac8cb1b387dadf02294abb16141f63
Dispersioned <dispersioned@gmail.com>
2022-08-26 20:09:02 +0300

    feat: catalog page basic logic

---
+++ frontend/src/entities/catalog/model.ts
@@ -0,0 +1,26 @@
+import { createEffect, createEvent, createStore, sample } from 'effector';
+import { mockFetch } from 'shared/helpers';
+import { productViewsMock } from 'shared/mocks/productsViews';
+import { ProductView } from 'shared/types';
+
+export const fetchProducts = createEvent();
+export const fetchProductsFx = createEffect<void, ProductView[]>();
+
+fetchProductsFx.use(async () => {
+  const data = await mockFetch<ProductView[]>(productViewsMock, 800);
+  return data;
+});
+
+export const $catalogProducts = createStore<ProductView[] | null>(null);
+const $isFetched = createStore(false);
+export const $isLoading = createStore(false);
+
+$catalogProducts.on(fetchProductsFx.doneData, (_, payload) => payload);
+$isFetched.on(fetchProductsFx.doneData, () => true);
+$isLoading.on(fetchProductsFx.pending, (_, payload) => payload);
+
+sample({
+  clock: fetchProducts,
+  filter: fetchProductsFx.pending.map((is) => !is),
+  target: fetchProductsFx,
+});
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,10 +1,21 @@
 import { IconButton } from '@mui/material';
 import { ReactComponent as SearchIcon } from 'assets/search.svg';
+import { useUnit } from 'effector-react';
+import { $catalogProducts, $isLoading, fetchProducts, fetchProductsFx } from 'entities/catalog';
+import { ProductCard } from 'entities/product';
+import { useEffect } from 'react';
 import { Layout } from 'shared/ui/Layout';
 
 import { ContentLayout, GridArea, Search, Title } from './styles';
 
 export const Home = () => {
+  const products = useUnit($catalogProducts);
+  const loading = useUnit($isLoading);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <Layout>
       <ContentLayout>
@@ -25,6 +36,13 @@ export const Home = () => {
               ),
             }}
           />
+        </GridArea>
+        <GridArea name="content">
+          {loading ? (
+            <div>loading</div>
+          ) : (
+            products?.map((product) => <ProductCard key={product.id} product={product} />)
+          )}
         </GridArea>
       </ContentLayout>
     </Layout>
