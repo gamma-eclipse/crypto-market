cdc4e783533fc567639f8bbaa4930baef08dacb4
Dispersioned <dispersioned@gmail.com>
2022-08-26 19:11:08 +0300

    feat: basic product card

---
+++ frontend/src/entities/product/ui/ProductCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProductView } from 'shared/types';
+
+type ProductCardProps = {
+  product: ProductView;
+};
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return <div />;
+};
