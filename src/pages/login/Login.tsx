eb937f67fedf70215a86d5e2a66cb7210010c618
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-09-01 17:32:22 +0300

    ref: extract styles

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,39 +1,7 @@
-import { Button, Card, CardContent, TextField, Typography, styled } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { COLORS } from 'shared/config/colors';
 
-export const ContentLayout = styled('div')`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: ${COLORS.bg[200]};
-`;
-
-export const LoginCard = styled(Card)`
-  max-width: 600px;
-  text-align: center;
-`;
-
-export const LoginLayout = styled(CardContent)`
-  display: grid;
-  justify-content: center;
-  grid-gap: 20px;
-`;
-
-export const ActionArea = styled('div')`
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const ActionBtn = styled(Button)`
-  width: 150px;
-  justify-self: center;
-`;
-
-export const ExtraArea = styled('div')`
-  text-align: start;
-`;
+import { ActionArea, ActionBtn, ContentLayout, ExtraArea, LoginCard, LoginLayout } from './styles';
 
 export const Login = () => {
   return (
---
+++ src/pages/login/styles.tsx
@@ -0,0 +1,35 @@
+import { Button, Card, CardContent, styled } from '@mui/material';
+import { COLORS } from 'shared/config/colors';
+
+export const ContentLayout = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${COLORS.bg[200]};
+`;
+
+export const LoginCard = styled(Card)`
+  max-width: 600px;
+  text-align: center;
+`;
+
+export const LoginLayout = styled(CardContent)`
+  display: grid;
+  justify-content: center;
+  grid-gap: 20px;
+`;
+
+export const ActionArea = styled('div')`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ActionBtn = styled(Button)`
+  width: 150px;
+  justify-self: center;
+`;
+
+export const ExtraArea = styled('div')`
+  text-align: start;
+`;
