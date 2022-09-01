031207f0e48463bb95e0c7317c150a393c26067c
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-09-01 17:25:37 +0300

    feat: modify ui

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,13 @@
 import { Button, Card, CardContent, TextField, Typography, styled } from '@mui/material';
-import { Layout } from 'shared/ui/Layout';
+import { Link } from 'react-router-dom';
+import { COLORS } from 'shared/config/colors';
 
 export const ContentLayout = styled('div')`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: 100vh;
+  background-color: ${COLORS.bg[200]};
 `;
 
 export const LoginCard = styled(Card)`
@@ -19,24 +21,44 @@ export const LoginLayout = styled(CardContent)`
   grid-gap: 20px;
 `;
 
-export const LoginBtn = styled(Button)`
+export const ActionArea = styled('div')`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ActionBtn = styled(Button)`
   width: 150px;
   justify-self: center;
 `;
 
+export const ExtraArea = styled('div')`
+  text-align: start;
+`;
+
 export const Login = () => {
   return (
-    <Layout>
-      <ContentLayout>
-        <LoginCard>
-          <LoginLayout>
-            <Typography variant="h5">Login</Typography>
-            <TextField variant="outlined" fullWidth label="Username" />
-            <TextField variant="outlined" fullWidth label="Password" type="password" />
-            <LoginBtn variant="contained">Go in</LoginBtn>
-          </LoginLayout>
-        </LoginCard>
-      </ContentLayout>
-    </Layout>
+    <ContentLayout>
+      <LoginCard>
+        <LoginLayout>
+          <Typography variant="h5">Login</Typography>
+          <TextField variant="outlined" fullWidth label="Username" />
+          <TextField variant="outlined" fullWidth label="Password" type="password" />
+          <ActionArea>
+            <ActionBtn variant="contained" color="secondary">
+              Go back
+            </ActionBtn>
+            <ActionBtn variant="contained">Go in</ActionBtn>
+          </ActionArea>
+          <ExtraArea>
+            <Typography>
+              Don&apos;t have one? <Link to="/register">Register</Link>
+            </Typography>
+            <Typography>
+              I <Link to="/recover">forgot</Link> my password.
+            </Typography>
+          </ExtraArea>
+        </LoginLayout>
+      </LoginCard>
+    </ContentLayout>
   );
 };
